09e308d08c5debc18e1d54df56ef41451c9ac5a2
igorloss <igor.loss@ifood.com.br>
2023-07-23 18:47:58 -0300

    adicionando o exercicio 7 resolvido

--- Exercicios/src/ExercicioSete.java
+++ Exercicios/src/ExercicioSete.java
@@ -1,2 +1,19 @@
+import java.util.Scanner;
 public class ExercicioSete {
+    public static void main(String[] args){
+
+        //Instanciando um novo scaner
+        Scanner s = new Scanner(System.in);
+
+        //Exibindo mensagem no console
+        System.out.println("Digite um número: ");
+
+        //Recebendo o numero digitado na variável primitiva inteiro:
+        int numero = s.nextInt();
+        Integer numeroI = (Integer) numero;
+
+        //Imprimindo o número no console
+        System.out.println("O número convertido para o tipo wrapper: " + numeroI);
+
+    }
 }
