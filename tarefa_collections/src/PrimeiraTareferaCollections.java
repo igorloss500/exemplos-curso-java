d69d1ddbcad0355e0a595bb3c80c1ae3cda86f11
igorloss <igor.loss@ifood.com.br>
2023-08-02 11:07:53 -0300

    adicionando a primeira parte do exercício resolvido

--- tarefa_collections/src/PrimeiraTareferaCollections.java
+++ tarefa_collections/src/PrimeiraTareferaCollections.java
@@ -1,2 +1,27 @@
+import java.util.*;
+
 public class PrimeiraTareferaCollections {
+    public static void main(String args[]){
+
+        //recebendo dados do console
+        Scanner sc = new Scanner(System.in);
+        String nome = sc.nextLine();
+
+        //intanciando a lista de nomes
+        List<String> lista = new ArrayList<>();
+
+        //obtendo cada nome da entrada utilizando split
+        String[] nomesSeparados = nome.split(",");
+
+        //Salvando cada item na lista de nomes
+        for(String item : nomesSeparados){
+            lista.add(item);
+        }
+
+        //Ordenando a lista de nomes por ordem alfabética
+        Collections.sort(lista);
+
+        //imprimindo no console
+        System.out.println(lista);
+    }
 }
